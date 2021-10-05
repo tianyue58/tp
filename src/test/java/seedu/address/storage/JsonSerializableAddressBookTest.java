6237288512c359adcccfacb1727d3de70c59ecf9
tianyue58 <e0563952@u.nus.edu>
2021-10-05 09:43:38 +0800

    Fix checkstyle

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -1,14 +1,14 @@
 package seedu.address.storage;
 
-import static seedu.address.testutil.Assert.assertThrows;
+//import static seedu.address.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.junit.jupiter.api.Test;
-
-import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.commons.util.JsonUtil;
+//import org.junit.jupiter.api.Test;
+//
+//import seedu.address.commons.exceptions.IllegalValueException;
+//import seedu.address.commons.util.JsonUtil;
 
 public class JsonSerializableAddressBookTest {
 
