939b1c9cee244f8ab59b42253feb0c72f1fce5f0
LEVONO-V15\Nature <hutianran711@outlook.com>
2021-10-08 16:23:08 +0800

    Update implementions in Completion class

--- src/main/java/seedu/address/model/application/Completion.java
+++ src/main/java/seedu/address/model/application/Completion.java
@@ -1,12 +1,17 @@
 package seedu.address.model.application;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents an Application's completion status in InternSHIP.
  * ADD THE GUARANTEE LATER
  */
 public class Completion {
+
+    public static final String MESSAGE_CONSTRAINTS = "Completion can be 'Completed' or 'Uncompleted'."
+            + "It will be 'Uncompleted' by default for a new Application that is added.";
+
     public final String value;
 
     /**
@@ -14,9 +19,24 @@ public class Completion {
      *
      * @param status A valid completion status.
      */
+
+    /*
+     * The first character of the completion must not be
+     * a whitespace.
+     */
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+
     public Completion(String status) {
         requireNonNull(status);
+        checkArgument(isValidCompletion(status), MESSAGE_CONSTRAINTS);
         value = status;
+    }
+
+    /**
+     * Returns true if a given string is a valid status.
+     */
+    public static boolean isValidCompletion(String test) {
+        return test.matches(VALIDATION_REGEX);
     }
 
     @Override
