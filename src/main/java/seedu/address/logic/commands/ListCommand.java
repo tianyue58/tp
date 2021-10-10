3aeab0f9009b061820383f6b215d7a285cdfa7e5
Nature711 <e0665488@u.nus.edu>
2021-10-10 21:06:42 +0800

    Refine displayed message for Find and List Command

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,12 +1,13 @@
 package seedu.address.logic.commands;
 
+import seedu.address.commons.core.Messages;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_APPLICATIONS;
 
 import seedu.address.model.Model;
 
 /**
- * Lists all persons in the address book to the user.
+ * Lists all applications in InternSHIP to the user.
  */
 public class ListCommand extends Command {
 
@@ -14,11 +15,11 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all applications";
 
-
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredApplicationList(PREDICATE_SHOW_ALL_APPLICATIONS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        int listSize = model.getFilteredApplicationList().size();
+        return new CommandResult(listSize == 0 ? Messages.MESSAGE_EMPTY_LIST : MESSAGE_SUCCESS);
     }
 }
