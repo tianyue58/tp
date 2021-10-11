99625f8b1c2410016f355fbdeb049cc69f9afd7d
Nature711 <e0665488@u.nus.edu>
2021-10-11 16:46:31 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/logic/parser/CompleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CompleteCommandParser.java
@@ -9,7 +9,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses input arguments and creates a new CompleteCommand object
  */
-public class CompleteCommandParser implements Parser<CompleteCommand>{
+public class CompleteCommandParser implements Parser<CompleteCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the CompleteCommand
