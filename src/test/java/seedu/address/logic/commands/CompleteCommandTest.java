8da3f5c134b64d02de902b67e25f6a710a13bce2
Nature711 <e0665488@u.nus.edu>
2021-10-11 21:31:15 +0800

    Update Complete Command Test

--- src/test/java/seedu/address/logic/commands/CompleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CompleteCommandTest.java
@@ -1,6 +1,19 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalApplications.getTypicalInternship;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_APPLICATION;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_APPLICATION;
+
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -12,40 +25,40 @@ import seedu.address.model.application.Position;
 import seedu.address.model.application.Status;
 import seedu.address.model.tag.Tag;
 
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalApplications.getTypicalInternship;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_APPLICATION;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_APPLICATION;
-
 public class CompleteCommandTest {
 
     private Model model = new ModelManager(getTypicalInternship(), new UserPrefs());
 
-//    @Test
-//    public void execute_validIndexUnfilteredList_success() {
-//        Application applicationToComplete = model.getFilteredApplicationList()
-//                .get(INDEX_FIRST_APPLICATION.getZeroBased());
-//        CompleteCommand completeCommand = new CompleteCommand(INDEX_FIRST_APPLICATION);
-//
-//        String expectedMessage = String.format(CompleteCommand.MESSAGE_SUCCESS, applicationToComplete);
-//
-//        ModelManager expectedModel = new ModelManager(model.getInternship(), new UserPrefs());
-//        Company company = applicationToComplete.getCompany();
-//        Position position = applicationToComplete.getPosition();
-//        Deadline deadline = applicationToComplete.getDeadline();
-//        Status status = applicationToComplete.getStatus();
-//        Set<Tag> tagList = applicationToComplete.getTags();
-//        Completion completion = new Completion("Completed");
-//        Application completedApplication = new Application(company, position, deadline, status, tagList, completion);
-//
-//        expectedModel.setApplication(applicationToComplete, completedApplication);
-//
-//        assertCommandSuccess(completeCommand, model, expectedMessage, expectedModel);
-//    }
+    @Test
+    public void execute_validIndexApplicationList_success() {
+        Application applicationToComplete = model.getFilteredApplicationList()
+                .get(INDEX_FIRST_APPLICATION.getZeroBased());
+
+        Company company = applicationToComplete.getCompany();
+        Position position = applicationToComplete.getPosition();
+        Deadline deadline = applicationToComplete.getDeadline();
+        Status status = applicationToComplete.getStatus();
+        Set<Tag> tagList = applicationToComplete.getTags();
+        Completion completion = new Completion("Completed");
+        Application completedApplication = new Application(company, position, deadline, status, tagList, completion);
+
+        String expectedMessage = String.format(CompleteCommand.MESSAGE_SUCCESS, completedApplication);
+        ModelManager expectedModel = new ModelManager(model.getInternship(), new UserPrefs());
+        expectedModel.setApplication(applicationToComplete, completedApplication);
+
+        CompleteCommand completeCommand = new CompleteCommand(INDEX_FIRST_APPLICATION);
+
+        assertCommandSuccess(completeCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndexApplicationList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredApplicationList().size() + 1);
+        CompleteCommand completeCommand = new CompleteCommand(outOfBoundIndex);
+
+        assertCommandFailure(completeCommand, model, Messages.MESSAGE_INVALID_APPLICATION_DISPLAYED_INDEX);
+    }
+
 
     @Test
     public void equals() {
