dc065bcb97291f1caa1710ece9949231652b823a
tianyue58 <e0563952@u.nus.edu>
2021-10-26 11:54:41 +0800

    Revert "Revert "Update find command to include interview time field""
    This reverts commit 929953836a8f2f2c2924ed10b5406d60550ec35c.

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPANY_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPLETION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DEADLINE_OF_APPLICATION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERNSHIP_POSITION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERVIEW_DATE_AND_TIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PRIORITY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REQUIREMENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
@@ -16,6 +17,7 @@ import seedu.address.model.Model;
 import seedu.address.model.application.Application;
 import seedu.address.model.application.CompletionContainsKeywordsPredicate;
 import seedu.address.model.application.DeadlineContainsKeywordsPredicate;
+import seedu.address.model.application.InterviewContainsKeywordsPredicate;
 import seedu.address.model.application.NameContainsKeywordsPredicate;
 import seedu.address.model.application.PositionContainsKeywordsPredicate;
 import seedu.address.model.application.PriorityContainsKeywordsPredicate;
@@ -32,22 +34,26 @@ public class FindCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Finds all applications whose names, positions, deadline, "
-            + "completion, status, priority or requirements contain any of "
+            + "requirements, interview time, priority, completion or status contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters:\n"
             + PREFIX_COMPANY_NAME + "COMPANY_NAME "
             + PREFIX_INTERNSHIP_POSITION + "INTERNSHIP_POSITION "
             + PREFIX_DEADLINE_OF_APPLICATION + "DEADLINE_OF_APPLICATION "
+            + PREFIX_REQUIREMENT + "APPLICATION_REQUIREMENTS"
+            + PREFIX_INTERVIEW_DATE_AND_TIME + "INTERVIEW_TIME"
+            + PREFIX_PRIORITY + "PRIORITY"
             + PREFIX_COMPLETION + "APPLICATION_COMPLETION "
             + PREFIX_STATUS + "APPLICATION_STATUS\n"
             + "Example:\n"
             + "Find by company name: " + COMMAND_WORD + " " + PREFIX_COMPANY_NAME + "Amazon ByteDance Google\n"
             + "Find by position: " + COMMAND_WORD + " " + PREFIX_INTERNSHIP_POSITION + "engineer\n"
             + "Find by deadline: " + COMMAND_WORD + " " + PREFIX_DEADLINE_OF_APPLICATION + "2021-11-12\n"
-            + "Find by completion: " + COMMAND_WORD + " " + PREFIX_COMPLETION + "uncompleted\n"
-            + "Find by status: " + COMMAND_WORD + " " + PREFIX_STATUS + "accepted\n"
+            + "Find by requirement: " + COMMAND_WORD + " " + PREFIX_REQUIREMENT + "cv\n"
+            + "Find by interview time: " + COMMAND_WORD + " " + PREFIX_INTERVIEW_DATE_AND_TIME + "2021-12-16 1400\n"
             + "Find by priority: " + COMMAND_WORD + " " + PREFIX_PRIORITY + "High\n"
-            + "Find by requirement: " + COMMAND_WORD + " " + PREFIX_REQUIREMENT + "cv\n";
+            + "Find by completion: " + COMMAND_WORD + " " + PREFIX_COMPLETION + "uncompleted\n"
+            + "Find by status: " + COMMAND_WORD + " " + PREFIX_STATUS + "accepted\n";
 
 
     public static final String MESSAGE_NO_MATCHING = "No matching result found in your Internship list";
@@ -55,51 +61,58 @@ public class FindCommand extends Command {
     private final Predicate<Application> predicate;
 
     /**
-     * Creates a FindCommand to find the specified {@code Application}
+     * Creates a FindCommand to find the specified {@code Application} by name
      */
     public FindCommand(NameContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
     }
 
     /**
-     * Creates a FindCommand to find the specified {@code Application}
+     * Creates a FindCommand to find the specified {@code Application} by position
      */
     public FindCommand(PositionContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
     }
 
     /**
-     * Creates a FindCommand to find the specified {@code Application}
+     * Creates a FindCommand to find the specified {@code Application} by deadline
      */
     public FindCommand(DeadlineContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
     }
 
     /**
-     * Creates a FindCommand to find the specified {@code Application}
+     * Creates a FindCommand to find the specified {@code Application} by completion
      */
     public FindCommand(CompletionContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
     }
 
     /**
-     * Creates a FindCommand to find the specified {@code Application}
+     * Creates a FindCommand to find the specified {@code Application} by status
      */
     public FindCommand(StatusContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
     }
 
     /**
-     * Creates a FindCommand to find the specified {@code Application}
+     * Creates a FindCommand to find the specified {@code Application} by priority
      */
     public FindCommand(PriorityContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
     }
 
     /**
-     * Creates a FindCommand to find the specified {@code Application}
+     * Creates a FindCommand to find the specified {@code Application} by requirement
      */
     public FindCommand(RequirementsContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    /**
+     * Creates a FindCommand to find the specified {@code Application} by interview time
+     */
+    public FindCommand(InterviewContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
     }
 
