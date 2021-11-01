9c7388dfc7b04a78dc0a805ec5b402835388c72c
Nature711 <e0665488@u.nus.edu>
2021-11-01 09:48:23 +0800

    Fix testcases

--- src/main/java/seedu/address/model/application/Requirement.java
+++ src/main/java/seedu/address/model/application/Requirement.java
@@ -9,14 +9,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Requirement {
     public static final String MESSAGE_CONSTRAINTS =
-            "A requirement should only contain alphanumeric characters and space. "
-                    + "It should not be blank or longer than 20 characters.";
+            "A requirement should not be blank or longer than 20 characters.";
 
     /*
      * The first character of the position must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[^\\s].*";
 
 
     public final String value;
