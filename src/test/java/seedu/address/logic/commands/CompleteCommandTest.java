0ca452f6a462dea0f671924256408b9c6bbc3ad6
Willy Angga <ampedwilly@gmail.com>
2021-11-01 23:11:02 +0800

    Fix checkStyle

--- src/test/java/seedu/address/logic/commands/CompleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CompleteCommandTest.java
@@ -13,7 +13,6 @@ import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
