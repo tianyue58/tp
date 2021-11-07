ef32a578479046d3858cf2268c59d246cc57bfb1
Nature711 <e0665488@u.nus.edu>
2021-11-07 12:14:02 +0800

    Increase line coverage for find command

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,6 +1,14 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_COMPLETION_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_POSITION_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_PRIORITY_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_REQUIREMENT_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_STATUS_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMAZON;
+import static seedu.address.logic.commands.CommandTestUtil.POSITION_DESC_AMAZON;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPANY_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPLETION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERNSHIP_POSITION;
@@ -9,16 +17,22 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_REQUIREMENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.FindCommandParser.COMPLETION_MESSAGE_CONSTRAINTS;
+import static seedu.address.logic.parser.FindCommandParser.PRIORITY_MESSAGE_CONSTRAINTS;
+import static seedu.address.logic.parser.FindCommandParser.STATUS_MESSAGE_CONSTRAINTS;
 
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.model.application.Company;
 import seedu.address.model.application.CompletionContainsKeywordsPredicate;
 import seedu.address.model.application.NameContainsKeywordsPredicate;
+import seedu.address.model.application.Position;
 import seedu.address.model.application.PositionContainsKeywordsPredicate;
 import seedu.address.model.application.PriorityContainsKeywordsPredicate;
+import seedu.address.model.application.Requirement;
 import seedu.address.model.application.RequirementsContainsKeywordsPredicate;
 import seedu.address.model.application.StatusContainsKeywordsPredicate;
 
@@ -28,7 +42,15 @@ public class FindCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_moreThanOnePrefix_throwsParseException() {
+
+        assertParseFailure(parser, NAME_DESC_AMAZON + POSITION_DESC_AMAZON,
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
     }
 
     @Test
@@ -39,7 +61,8 @@ public class FindCommandParserTest {
         assertParseSuccess(parser, " " + PREFIX_COMPANY_NAME + "Google Shopee", expectedFindCommand);
 
         // multiple whitespaces between keywords
-        assertParseSuccess(parser, " " + PREFIX_COMPANY_NAME + " \n Google \n \t Shopee  \t", expectedFindCommand);
+        assertParseSuccess(parser, " " + PREFIX_COMPANY_NAME + " \n Google \n \t Shopee  \t",
+                expectedFindCommand);
     }
 
     @Test
@@ -99,4 +122,29 @@ public class FindCommandParserTest {
         assertParseSuccess(parser, " " + PREFIX_REQUIREMENT
                 + " \n cv \n \t resume  \t", expectedFindCommand);
     }
+
+    @Test
+    public void parse_invalidArgument_throwsParseException() {
+
+        //invalid company name
+        assertParseFailure(parser, INVALID_NAME_DESC, Company.MESSAGE_CONSTRAINTS);
+
+        //invalid position
+        assertParseFailure(parser, INVALID_POSITION_DESC, Position.MESSAGE_CONSTRAINTS);
+
+        //invalid priority
+        assertParseFailure(parser, INVALID_PRIORITY_DESC, PRIORITY_MESSAGE_CONSTRAINTS);
+
+        //invalid completion
+        assertParseFailure(parser, INVALID_COMPLETION_DESC, COMPLETION_MESSAGE_CONSTRAINTS);
+
+        //invalid status
+        assertParseFailure(parser, INVALID_STATUS_DESC, STATUS_MESSAGE_CONSTRAINTS);
+
+        //invalid requirement
+        assertParseFailure(parser, INVALID_REQUIREMENT_DESC, Requirement.MESSAGE_CONSTRAINTS);
+
+    }
+
+
 }
